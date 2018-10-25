c2ed40a3567f5e02ca56e29506483f99d7ff03a5
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-10-25 12:05:14 +0200

    Add files via upload

---
+++ python/Suspender m03.py
@@ -0,0 +1,10 @@
+## Python 3.6 ##
+**Suspender m03**
+
+pelo = input("de que color es tu pelo?")
+ojos = input("de que color son tus ojos?")
+if (pelo == "moreno")and(ojos == "marrones"):
+    print("Suspendido.")
+else:
+    print("Puedes_llegar_a_aprobar")    
+    
