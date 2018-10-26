828585249cd3391847d1cdd51ddcb3f5a5e8f9e0
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-10-26 12:50:50 +0200

    Add files via upload

---
+++ python/Practica1_Edats.py
@@ -0,0 +1,8 @@
+## Python 3.6 ##
+**Practica 1: Edats**
+
+edat = int (input("Quina edat tens?"))
+if(edat <5) or (edat >=65):
+    print("Gratis")
+else:
+    print("Pagar 2'5€")
---
+++ python/Practica2_Jubilats.py
@@ -0,0 +1,13 @@
+## Python 3.6 ##
+**Practica 2: Jubilats, sexe i rosses**
+
+jubilat = str (input("Ets jubilat? Si o no"))
+sexe = str(input("Ets home o dona? H o D"))
+cabell = str(input("De que color son els teus cabells?"))
+if(jubilat =="Si")or((sexe == "rossa")and(jubilat=="No")):
+    print("Gratis")
+else:
+    if(sexe==H):
+        print("Pagar 1€")
+    else:
+        print("Pagar 0.5€")
