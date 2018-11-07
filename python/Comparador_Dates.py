0fabf66852d205dc6f05662c4f35d39d23c66bbc
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-07 12:01:33 +0100

    Update Comparador_Dates.py

--- python/Comparador_Dates.py
+++ python/Comparador_Dates.py
@@ -22,41 +22,49 @@ else:
                     
 **************************************************************************************************
                              ##Python 3.6##
-                    """Comparador d'anys amb negatius"""
+                    """Comparador d'anys amb negatius sense errors"""
 #Coding: utf-8
 Actual= int(input("Indique el año actual: "))
 Futuro= int(input("Pon un año cualquiera: "))
-if(Actual == 0):
-    print("El año actual no puede ser 0")
+if(Actual == 0 and Futuro == 0):
+    print("El año actual y el año cualquiera no pueden ser 0, saludos.")
 else:
-    if(Futuro == 0):
-        print("El año cualquiera no puede ser 0")
+    if(Actual == 0):
+        print("El año actual no puede ser 0")
     else:
-        if(Actual < Futuro):
-            res_act=(Futuro - Actual)
-            if(res_act <= 0):
-                if(Actual < 0 and Future < 0):
-                    print("Para llegar al año ",Futuro," faltan ",res_act," años")
-                else:
-                    neg_act= (res_act + 1)
-                    if(neg_act == 0):
-                        print("Para llegar al año ",Futuro," falta 1 año")
-            else:
-                print("Para llegar al año ",Futuro," faltan ",res_act," años")
+        if(Futuro == 0):
+            print("El año cualquiera no puede ser 0")
         else:
-            if(Actual > Futuro):
-                res_fut=(Actual - Futuro)
-                if(Actual <= 0 or Futuro <=0):
-                    neg_fut= (res_fut - 1)
-                    if(neg_fut == 0):
-                        print("Desde el año ",Futuro," ha pasado 1 año")
+            if(Actual < Futuro):
+                res_act=(Futuro - Actual)
+                if(res_act <= 0):
+                    if(Actual < 0 and Future < 0):
+                        print("Para llegar al año ",Futuro," faltan ",res_act," años")
                     else:
-                        if(Actual <= 0 and Futuro <= 0):
-                            print("Desde el ",Futuro," han pasado ",res_fut," años")
-                        else:
-                            print("Desde el año ",Futuro," han pasado ",neg_fut," años")
+                        neg_act= (res_act + 1)
+                        if(neg_act == 0):
+                            print("Para llegar al año ",Futuro," falta 1 año")
                 else:
-                    print("Desde el ",Futuro," han pasado ",res_fut," años")
+                    print("Para llegar al año ",Futuro," faltan ",res_act," años")
             else:
-                if(Actual == Futuro):
-                    print("Son el mismo año!")
+                if(Actual > Futuro):
+                    res_fut=(Actual - Futuro)
+                    if(Actual <= 0 or Futuro <=0):
+                        neg_fut= (res_fut - 1)
+                        if(neg_fut == 0):
+                            print("Desde el año ",Futuro," ha pasado 1 año")
+                        else:
+                            if(Actual <= 0 and Futuro <= 0):
+                                print("Desde el ",Futuro," han pasado ",res_fut," años")
+                            else:
+                                print("Desde el año ",Futuro," han pasado ",neg_fut," años")
+                    else:
+                        print("Desde el ",Futuro," han pasado ",res_fut," años")
+                else:
+                    if(Actual == Futuro):
+                        print("Son el mismo año!")
+                        
+**************************************************************************************************
+                             ##Python 3.6##
+                    """Comparador d'anys amb rangs"""
+    
