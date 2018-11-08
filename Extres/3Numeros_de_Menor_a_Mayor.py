34986c0c51d79fdef322396ad20847316a9df67b
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-08 12:22:01 +0100

    Create 3Numeros_de_Menor_a_Mayor.py

---
+++ Extres/3Numeros_de_Menor_a_Mayor.py
@@ -0,0 +1,28 @@
+ ##Python 3.6##
+                 """3 Numeros ordenats de menor a mayor"""
+#Coding: Utf-8
+num1=int(input("1.Posa un nombre enter: "))
+num2=int(input("2.Posa un nombre enter: "))
+num3=int(input("3.Posa un nombre enter: "))
+if(num3 <= num2 and num2 <= num1):
+    print("",num3,",",num2,",",num1,"")
+elif(num3 >= num2 and num2 >= num1):
+    print("",num1,",",num2,",",num3,"")
+elif(num2 <= num1 and num1 <= num3):
+    print("",num2,",",num1,",",num3,"")
+elif(num1 >= num3 and num3 >= num2):
+    print("",num2,",",num3,",",num1,"")
+elif(num2 >= num1 and num1 >= num3):
+    print("",num3,",",num1,",",num2,"")
+elif(num2 >= num3 and num3 >= num1):
+    print("",num1,",",num3,",",num2,"")
+print("(Ordenats de menor a mayor)")
+
+***********************************************************
+                      ##Python 3.6##
+         """3 Numeros ordenados de menor a mayor sin IF"""
+##Creditos Ian##
+#coding:utf-8##
+numeros=list(eval(input("Posa tres numeros separats per comes: ")))
+numeros.sort()
+print(numeros)
