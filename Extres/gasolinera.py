16003e1a1e9d25b96ffe64b7e708beaaddc23d72
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-12 13:12:03 +0100

    Create gasolinera.py

---
+++ Extres/gasolinera.py
@@ -0,0 +1,9 @@
+#Python 3.6#
+"""Tipo de gasolineras"""
+#Coding: Utf-8
+gasolina= input("Qué gasolina quieres? (Super, Sin plomo o Diesel)")
+if(gasolina == Super):
+    print("Hay de dos tipos Normal (1'5€) y Turbo (1.55€) estas seguro de seguir con esta operacion?")
+    seguir= input("Seguimos con la operacion? ")
+    if(seguir == "Si" or seguir == "Yes")
+        
