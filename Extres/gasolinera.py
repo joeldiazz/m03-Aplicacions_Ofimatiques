17c845ce20cad356a6069f20c41f529cf15adf1f
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-12 13:41:08 +0100

    Update gasolinera.py

--- Extres/gasolinera.py
+++ Extres/gasolinera.py
@@ -4,6 +4,28 @@
 gasolina= input("Qué gasolina quieres? (Super, Sin plomo o Diesel)")
 if(gasolina == Super):
     print("Hay de dos tipos Normal (1'5€) y Turbo (1.55€) estas seguro de seguir con esta operacion?")
-    seguir= input("Seguimos con la operacion? ")
+    seguir= input("Seguimos con la operacion?(Si o No) ")
     if(seguir == "Si" or seguir == "Yes")
-        
+        super= input("Quieres Normal o turbo?")
+        if(super == "Normal"):
+            print("El normal són 1.5€, gracias.")
+        else:
+            print("El turbo són 1.55€, gracias.")
+elif(gasolina == "Sin plomo")
+    print("Hay de dos tipos Normal 1.6€ con aditivos 1.65€ estas seguro de seguir con esta operacion?")
+    seguir= input("Seguimos con la operacion?(Si o No) ")
+    if(seguir == "Si" or seguir == "Yes")
+        sin= input("Quieres Normal o con aditivos(sabor naranja) ?")
+        if(sin == "Normal"):
+            print("El normal son 1.6€, gracias.")
+        else:
+            print("Con aditivos son 1.65€, gracias.")
+elif(gasolina == "Diesel")
+    print("Hay de dos tipos Normal 1.7€, Fast&Furius 1.75€ estas seguro de seguir con esta operacion?")
+    seguir= input("Seguimos con la operacion?(Si o No) ")
+    if(seguir == "Si" or seguir == "Yes")
+        sin= input("Quieres Normal o Fast&Furius? ")
+        if(sin == "Normal"):
+            print("El normal son 1.7€, gracias.")
+        else:
+            print("El Fast&Furius son 1.75€, gracias.")
