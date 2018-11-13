ec2743597a2c159609938152db3bcbdb19b24111
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-13 13:10:12 +0100

    Update calculo-areas.py

--- Extres/calculo-areas.py
+++ Extres/calculo-areas.py
@@ -14,3 +14,23 @@ elif(figura == "C"):
     print("Un circulo de radio",radio,"tiene un area de",areac,"")
 else:
     print("Tienes que poner T o C")
+
+    --------------------------------------------------------------------------
+    
+#Python 3.6#
+"""Calculo de areas redondeado"""
+#Coding: Utf-8
+figura = input("¿Qué figura quiere calcular? Triangulo (T) o Circulo (C): ")
+if(figura == "T"):
+    base = int(input("Escriba la base: "))
+    altura = int(input("Escriba la altura: "))
+    areat= ((base * altura)/2)
+    answert = str(round(areat, 2))
+    print("Un triángulo de base",base,"y altura",altura,"tiene un area de",answert,"")
+elif(figura == "C"):
+    radio = int(input("Escriba el radio: "))
+    areac = (3.14*(radio*radio))
+    answerc = str(round(areac, 2))
+    print("Un circulo de radio",radio,"tiene un area de",answerc,"")
+else:
+    print("Tienes que poner T o C")
