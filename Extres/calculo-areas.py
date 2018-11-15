e55408ff7e1169c3501b832b6f58160970bee6bf
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-15 11:58:12 +0100

    Update calculo-areas.py

--- Extres/calculo-areas.py
+++ Extres/calculo-areas.py
@@ -7,13 +7,20 @@ if(figura == "T"):
     base = float(input("Escriba la base: "))
     altura = float(input("Escriba la altura: "))
     areat= ((base * altura)/2)
-    print("Un triángulo de base",base,"y altura",altura,"tiene un area de",areat,"")
+    if(base == 0 or altura == 0):
+        print("La base/altura no puede ser 0")
+    else:
+        print("Un triángulo de base",base,"y altura",altura,"tiene un area de",areat,"")
 elif(figura == "C"):
     radio = float(input("Escriba el radio: "))
     areac = (3.14*(radio*radio))
-    print("Un circulo de radio",radio,"tiene un area de",areac,"")
+    if(radio == 0):
+        print("El radio no puede ser 0")
+    else:
+        print("Un circulo de radio",radio,"tiene un area de",areac,"")
 else:
     print("Tienes que poner T o C")
+   
 
     --------------------------------------------------------------------------
 
@@ -26,11 +33,17 @@ if(figura == "T"):
     altura = float(input("Escriba la altura: "))
     areat= ((base * altura)/2)
     answert = str(round(areat, 2))
-    print("Un triángulo de base",base,"y altura",altura,"tiene un area de",answert,"")
+    if(base == 0 or altura == 0):
+        print("La base/altura no puede ser 0")
+    else:
+        print("Un triángulo de base",base,"y altura",altura,"tiene un area de",answert,"")
 elif(figura == "C"):
     radio = float(input("Escriba el radio: "))
     areac = (3.14*(radio*radio))
     answerc = str(round(areac, 2))
-    print("Un circulo de radio",radio,"tiene un area de",answerc,"")
+    if(radio == 0):
+         print("El radio no puede ser 0")
+    else:
+        print("El radio no puede ser 0")print("Un circulo de radio",radio,"tiene un area de",answerc,"")
 else:
     print("Tienes que poner T o C")
