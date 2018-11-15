64c9b164ef1d23627e95669a456bb5ee293bdef0
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-15 10:21:54 +0100

    Update and rename Numero_Maxim_2Nombres to Numero_Maxim_2Nombres.py

--- Extres/Numero_Maxim_2Nombres.py
+++ Extres/Numero_Maxim_2Nombres.py
@@ -4,7 +4,7 @@
 #Coding: Utf-8
 num1 = float(input("1. Pon un número: "))
 num2 = float(input("2. Pon un número: "))
-if(num1 < num2 or num2 == num1):
+if(num1 < num2):
      print("",num2,"")
 else:
     print("",num1,"")
