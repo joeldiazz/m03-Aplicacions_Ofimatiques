dfc0e16276940c54ca5191cc1e8983f8aa596bc8
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-26 12:40:31 +0100

    Create pares.py

---
+++ python/pares.py
@@ -0,0 +1,8 @@
+#Python 3.6#
+"""Números pares e impares"""
+#Coding: Utf-8
+numero= int(input("Pon un numero: "))
+if(numero%2 == 0):
+    print("Qué bonito número par!")
+else:
+print("Que numero más vulgar...")
