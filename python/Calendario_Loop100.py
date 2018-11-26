cf3d6586c852b8ec3357e58358087a5c93dbc0f7
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-26 10:23:09 +0100

    Update Calendario_Loop100.py

--- python/Calendario_Loop100.py
+++ python/Calendario_Loop100.py
@@ -20,3 +20,23 @@ if(any == "Si"):
         input()
         for dia in range(300,365):
             print(dia+1)
+elif(any == "No")
+    for dia in range(0,100):
+        print(dia+1)
+    if(dia%100==100-1):       
+        print("Pulse Enter")
+        input()
+        for dia in range(100,200):
+            print(dia+1)
+    if(dia%200==200-1):       
+        print("Pulse Enter")
+        input()
+        for dia in range(200,300):
+            print(dia+1)
+    if(dia%300==300-1):       
+        print("Pulse Enter")
+        input()
+        for dia in range(300,366):
+            print(dia+1)
+else:
+    print("Debes escribir Si o No")
