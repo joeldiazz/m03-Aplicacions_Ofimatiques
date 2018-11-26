8e0922763de8691353893a387828a6659789de37
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-26 01:00:19 +0100

    Update Calendario_Real_Bisiesto.py

--- python/Calendario_Real_Bisiesto.py
+++ python/Calendario_Real_Bisiesto.py
@@ -4,25 +4,25 @@
 print("Enero!")
 for dia in range(0,366):
     print(dia+1)
-    if dia%366==31-1:
+    if (dia%366==31-1):
         print("Febrero!")
-    if dia%366==60-1:
+    if (dia%366==60-1):
         print("Marzo!")
-    if dia%366==91-1:
+    if (dia%366==91-1):
         print("Abril!")
-    if dia%366==121-1:
+    if (dia%366==121-1):
         print("Mayo!")
-    if dia%366==152-1:
+    if (dia%366==152-1):
         print("Junio!")
-    if dia%366==182-1:
+    if (dia%366==182-1):
         print("Julio!")
-    if dia%366==213-1:
+    if (dia%366==213-1):
         print("Agosto!")
-    if dia%366==244-1:
+    if (dia%366==244-1):
         print("Setiembre!")
-    if dia%366==274-1:
+    if (dia%366==274-1):
         print("Octubre!")
-    if dia%366==305-1:
+    if (dia%366==305-1):
         print("Noviembre!")
-    if dia%366==335-1:
+    if (dia%366==335-1):
         print("Diciembre!")
