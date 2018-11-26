7893e22ea3a2cd778c761d116f878287ccffbd5d
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-26 01:07:00 +0100

    Update Calendario_100dias.py

--- python/Calendario_100dias.py
+++ python/Calendario_100dias.py
@@ -5,24 +5,24 @@ any = input("Tu año es Bisiesto: ")
 if(any == "Si"):
     for dia in range(0,366):
         print(dia+1)
-        if dia%366==100-1:
+        if (dia%366==100-1):
             print("Primeros 100 Días!!!")
-        if dia%366==200-1:
+        if (dia%366==200-1):
             print("Muy bien sigue asi!! Ya 200")
-        if dia%366==300-1:
+        if (dia%366==300-1):
             print("Enhorabuena por tus 300 días ya queda poco!")
-        if dia%366==366-1:
+        if (dia%366==366-1):
             print("El año a acabado!")
 elif(any == "No"):
     for dia in range(0,365):
         print(dia+1)
-        if dia%365==100-1:
+        if (dia%365==100-1):
             print("Primeros 100 Días!!!")
-        if dia%365==200-1:
+        if (dia%365==200-1):
             print("Muy bien sigue asi!! Ya 200")
-        if dia%365==300-1:
+        if (dia%365==300-1):
             print("Enhorabuena por tus 300 días ya queda poco!")
-        if dia%365==365-1:
+        if (dia%365==365-1):
             print("El año a acabado!")
 else:
     print("Debes poner Si o No")
