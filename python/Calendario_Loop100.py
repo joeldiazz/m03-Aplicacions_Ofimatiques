9e2d02ac1fbcbd9d6de2cef16f268bc66857d804
Joel Díaz <44450349+joeldiazz@users.noreply.github.com>
2018-11-26 10:20:41 +0100

    Create Calendario_Loop100.py

---
+++ python/Calendario_Loop100.py
@@ -0,0 +1,22 @@
+##Python 3.6##
+#coding:utf-8
+"""Enter cada 100 dias"""
+any = input("Tu año es Bisiesto: ")
+if(any == "Si"):
+    for dia in range(0,100):
+        print(dia+1)
+    if(dia%100==100-1):       
+        print("Pulse Enter")
+        input()
+        for dia in range(100,200):
+            print(dia+1)
+    if(dia%200==200-1):       
+        print("Pulse Enter")
+        input()
+        for dia in range(200,300):
+            print(dia+1)
+    if(dia%300==300-1):       
+        print("Pulse Enter")
+        input()
+        for dia in range(300,365):
+            print(dia+1)
